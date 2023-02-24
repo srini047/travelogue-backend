3339de200c52dba4c4186fcd976ed7c26059ce45
srini047 <srinikethcr7@gmail.com>
2023-02-25 00:30:39 +0530

    fix: db connection

--- database/connection.js
+++ database/connection.js
@@ -1,4 +1,4 @@
-require('dotenv').config()
+require("dotenv").config();
 const mongoose = require("mongoose");
 
 const db = process.env.MONGO_DB_URL;
@@ -8,7 +8,7 @@ mongoose
     useUnifiedTopology: true,
   })
   .then(() => {
-    console.log(`connected to db `);
+    console.log(`Connected to database ${db}`);
   })
   .catch((err) => {
     console.log("error connecting to db", err);
