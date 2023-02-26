16eab2772576d88a50cf021ce1b3060357ee1994
srini047 <srinikethcr7@gmail.com>
2023-02-26 15:39:21 +0530

    new route

--- routes/getPlaces.js
+++ routes/getPlaces.js
@@ -4,9 +4,12 @@ const router = express.Router();
 const axios = require("axios");
 
 router.get("/places", async (req, res) => {
+  const latitude = req.query.latitude || 13.534534534;
+  const longitude = req.query.longitude || 45.4353545;
+  const limit = req.query.limit || 15;
   const config = {
     method: "get",
-    url: `https://api.geoapify.com/v2/places?type=tourism&lat=48.68573055&lon=10.81881479176283&limit=20&apiKey=${process.env.GEOAPIFY_PLACES_API}`,
+    url: `https://api.geoapify.com/v2/places?type=tourism&lat=${latitude}&lon=${longitude}&limit=${limit}&apiKey=${process.env.GEOAPIFY_PLACES_API}`,
     headers: {
       accept: "application/json",
       "content-type": "application/json",
@@ -15,8 +18,8 @@ router.get("/places", async (req, res) => {
 
   axios(config)
     .then(function (response) {
-      console.log(response.data);
-      res.send(response.data);
+      console.log(response.data.features);
+      res.send(response.data.features);
     })
     .catch(function (error) {
       console.log(error);
